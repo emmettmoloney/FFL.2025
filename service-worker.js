2392a43c6f0b5c7b925124407f0fbe17bc665c14
emmettmoloney <esm4551@gmail.com>
2026-03-15 14:34:04 -0500

    Add files via upload
    Implemented update checker.

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 // service-worker.js
-const CACHE_NAME = 'ffl-calc-v1.3';
-const VERSION = '1.3'
-const UPDATE_DATE = '3.15.26'
+const CACHE_NAME = 'ffl-calc-v1.4' /* UPDATE CACHE NAME		'ffl-calc-v1.x'; */
+const VERSION = '1.4' /* UPDATE VERSION		'1.x'		*/
+const UPDATE_DATE = '3.15.26' /*		'UPDATE DATE OF UPLOAD'		*/
 const urlsToCache = [
   './',
   './index.html',
